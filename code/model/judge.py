fe466656d539e251805ce3aef37fdfe635d1882b
Hoang Hung Manh <100746478+hungmanh21@users.noreply.github.com>
2024-11-08 15:50:33 +0700

    Update judge.py (anthropic)

--- code/model/judge.py
+++ code/model/judge.py
@@ -28,43 +28,55 @@ class Judge(tf.keras.Model):
         self.batch_size = params['batch_size'] * (1 + params['false_facts_train']) * params['num_rollouts']
         self.use_entity_embeddings = params['use_entity_embeddings']
 
-        # Initializers
-        self.entity_initializer = tf.keras.initializers.GlorotUniform() if params['use_entity_embeddings'] else tf.zeros_initializer()
+        # Initialize embeddings
+        self.initialize_embeddings()
         
-        # Define embeddings
-        self.define_embeddings()
+        # Initialize dense layers
+        self.hidden_layers_list = []
+        for i in range(self.hidden_layers - 1):
+            self.hidden_layers_list.append(
+                tf.keras.layers.Dense(self.hidden_size, activation='relu', name=f"layer_{i}")
+            )
+        self.hidden_layers_list.append(
+            tf.keras.layers.Dense(self.hidden_size, name=f"layer_{self.hidden_layers - 1}")
+        )
+        
+        # Classifier layers
+        self.classifier_1 = tf.keras.layers.Dense(self.hidden_size, activation='relu', name='classifier_judge_0')
+        self.classifier_2 = tf.keras.layers.Dense(1, name='classifier_judge_1')
 
-    def define_embeddings(self):
+    def initialize_embeddings(self):
         '''
-        Creates and adds the embeddings for the KG's relations and entities to the graph, as well as assign operations
-        needed when using pre-trained embeddings.
-    
-        :return: None
+        Creates embeddings for the KG's relations and entities
         '''
         # Relation embeddings
-        with tf.name_scope("judge"):
-            self.relation_embedding_placeholder = tf.compat.v1.placeholder(tf.float32,
-                                                                           [self.action_vocab_size, self.embedding_size])
-    
-            self.relation_lookup_table = self.add_weight(
-                name="relation_lookup_table",
-                shape=[self.action_vocab_size, self.embedding_size],
-                initializer=tf.keras.initializers.GlorotUniform(),
-                trainable=self.train_relations
-            )
-            self.relation_embedding_init = tf.compat.v1.assign(self.relation_lookup_table, self.relation_embedding_placeholder)
-    
+        self.relation_lookup_table = tf.keras.layers.Embedding(
+            input_dim=self.action_vocab_size,
+            output_dim=self.embedding_size,
+            embeddings_initializer='glorot_uniform',
+            trainable=self.train_relations,
+            name="relation_lookup_table"
+        )
+
         # Entity embeddings
-        with tf.name_scope("judge"):
-            self.entity_embedding_placeholder = tf.compat.v1.placeholder(tf.float32,
-                                                                         [self.entity_vocab_size, self.embedding_size])
-            self.entity_lookup_table = self.add_weight(
-                name="entity_lookup_table",
-                shape=[self.entity_vocab_size, self.embedding_size],
-                initializer=self.entity_initializer,
-                trainable=self.train_entities
-            )
-            self.entity_embedding_init = tf.compat.v1.assign(self.entity_lookup_table, self.entity_embedding_placeholder)
+        entity_initializer = 'glorot_uniform' if self.use_entity_embeddings else 'zeros'
+        self.entity_lookup_table = tf.keras.layers.Embedding(
+            input_dim=self.entity_vocab_size,
+            output_dim=self.embedding_size,
+            embeddings_initializer=entity_initializer,
+            trainable=self.train_entities,
+            name="entity_lookup_table"
+        )
+
+    def set_embeddings(self, relation_embeddings, entity_embeddings):
+        '''
+        Sets pre-trained embeddings
+        
+        :param relation_embeddings: numpy array of shape [action_vocab_size, embedding_size]
+        :param entity_embeddings: numpy array of shape [entity_vocab_size, embedding_size]
+        '''
+        self.relation_lookup_table.set_weights([relation_embeddings])
+        self.entity_lookup_table.set_weights([entity_embeddings])
 
     def set_labels(self, labels):
         '''
@@ -77,8 +89,8 @@ class Judge(tf.keras.Model):
         '''
         Encodes an action into its embedded representation.
         '''
-        relation_embedding = tf.nn.embedding_lookup(self.relation_lookup_table, next_relations)
-        entity_embedding = tf.nn.embedding_lookup(self.entity_lookup_table, next_entities)
+        relation_embedding = self.relation_lookup_table(next_relations)
+        entity_embedding = self.entity_lookup_table(next_entities)
         
         if self.use_entity_embeddings:
             action_embedding = tf.concat([relation_embedding, entity_embedding], axis=-1)
@@ -87,18 +99,25 @@ class Judge(tf.keras.Model):
 
         return action_embedding
 
+    @tf.function
     def extend_argument(self, argument, t, action_idx, next_relations, next_entities, range_arr):
         '''
         Extends an argument by adding the embedded representation of an action.
         '''
-        chosen_relation = tf.gather_nd(next_relations, tf.stack([range_arr, action_idx], axis=-1))
-        chosen_entities = tf.gather_nd(next_entities, tf.stack([range_arr, action_idx], axis=-1))
+        indices = tf.stack([range_arr, action_idx], axis=-1)
+        chosen_relation = tf.gather_nd(next_relations, indices)
+        chosen_entities = tf.gather_nd(next_entities, indices)
         action_embedding = self.action_encoder_judge(chosen_relation, chosen_entities)
 
-        argument = tf.cond(tf.equal(t % self.path_length, 0),
-                           lambda: action_embedding,
-                           lambda: tf.concat([argument, action_embedding], axis=-1))
-        return argument
+        def concat_embedding():
+            return tf.concat([argument, action_embedding], axis=-1)
+        
+        def return_embedding():
+            return action_embedding
+
+        return tf.cond(tf.equal(t % self.path_length, 0),
+                      return_embedding,
+                      concat_embedding)
 
     def classify_argument(self, argument):
         '''
@@ -108,14 +127,14 @@ class Judge(tf.keras.Model):
 
         # Reshape argument for the dense layer
         if self.use_entity_embeddings:
-            argument = tf.reshape(argument, shape=[-1, self.path_length * 2 * self.embedding_size + 2 * self.embedding_size])
+            argument = tf.reshape(argument, [-1, self.path_length * 2 * self.embedding_size + 2 * self.embedding_size])
         else:
-            argument = tf.reshape(argument, shape=[-1, self.path_length * self.embedding_size + 2 * self.embedding_size])
+            argument = tf.reshape(argument, [-1, self.path_length * self.embedding_size + 2 * self.embedding_size])
 
+        # Pass through hidden layers
         hidden = argument
-        for i in range(self.hidden_layers - 1):
-            hidden = tf.keras.layers.Dense(self.hidden_size, activation='relu', name=f"layer_{i}")(hidden)
-        hidden = tf.keras.layers.Dense(self.hidden_size, name=f"layer_{self.hidden_layers - 1}")(hidden)
+        for layer in self.hidden_layers_list:
+            hidden = layer(hidden)
         
         logits = self.get_logits_argument(hidden)
         return logits, hidden
@@ -124,8 +143,8 @@ class Judge(tf.keras.Model):
         '''
         Assigns logits to an argument.
         '''
-        logits = tf.keras.layers.Dense(self.hidden_size, activation='relu', name='classifier_judge_0')(argument)
-        logits = tf.keras.layers.Dense(1, name='classifier_judge_1')(logits)
+        logits = self.classifier_1(argument)
+        logits = self.classifier_2(logits)
         return logits
 
     def final_loss(self, rep_argu_list):
@@ -134,14 +153,16 @@ class Judge(tf.keras.Model):
         '''
         average_argu = tf.reduce_mean(tf.stack(rep_argu_list, axis=-1), axis=-1)
         final_logit = self.get_logits_argument(average_argu)
-        final_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.labels, logits=final_logit))
+        final_loss = tf.reduce_mean(
+            tf.keras.losses.binary_crossentropy(self.labels, final_logit, from_logits=True)
+        )
         
         return final_loss, final_logit
 
     def set_query_embeddings(self, query_subject, query_relation, query_object):
         '''
-        Sets the judge's query information to the placeholders used by trainer.
+        Sets the judge's query information.
         '''
-        self.query_subject_embedding = tf.nn.embedding_lookup(self.entity_lookup_table, query_subject)
-        self.query_relation_embedding = tf.nn.embedding_lookup(self.relation_lookup_table, query_relation)
-        self.query_object_embedding = tf.nn.embedding_lookup(self.entity_lookup_table, query_object)
+        self.query_subject_embedding = self.entity_lookup_table(query_subject)
+        self.query_relation_embedding = self.relation_lookup_table(query_relation)
+        self.query_object_embedding = self.entity_lookup_table(query_object)
