1e917b349d0856843d1176c27e7522facef67373
Hoang Hung Manh <100746478+hungmanh21@users.noreply.github.com>
2024-11-08 15:42:10 +0700

    Update judge.py to compatible with tensorflow 2.x

--- code/model/judge.py
+++ code/model/judge.py
@@ -69,4 +69,61 @@ class Judge(tf.keras.Model):
 
         return action_embedding
 
-    def extend_argument(self, argume
+    def extend_argument(self, argument, t, action_idx, next_relations, next_entities, range_arr):
+        '''
+        Extends an argument by adding the embedded representation of an action.
+        '''
+        chosen_relation = tf.gather_nd(next_relations, tf.stack([range_arr, action_idx], axis=-1))
+        chosen_entities = tf.gather_nd(next_entities, tf.stack([range_arr, action_idx], axis=-1))
+        action_embedding = self.action_encoder_judge(chosen_relation, chosen_entities)
+
+        argument = tf.cond(tf.equal(t % self.path_length, 0),
+                           lambda: action_embedding,
+                           lambda: tf.concat([argument, action_embedding], axis=-1))
+        return argument
+
+    def classify_argument(self, argument):
+        '''
+        Classifies arguments by computing a hidden representation and assigning logits.
+        '''
+        argument = tf.concat([argument, self.query_relation_embedding, self.query_object_embedding], axis=-1)
+
+        # Reshape argument for the dense layer
+        if self.use_entity_embeddings:
+            argument = tf.reshape(argument, shape=[-1, self.path_length * 2 * self.embedding_size + 2 * self.embedding_size])
+        else:
+            argument = tf.reshape(argument, shape=[-1, self.path_length * self.embedding_size + 2 * self.embedding_size])
+
+        hidden = argument
+        for i in range(self.hidden_layers - 1):
+            hidden = tf.keras.layers.Dense(self.hidden_size, activation='relu', name=f"layer_{i}")(hidden)
+        hidden = tf.keras.layers.Dense(self.hidden_size, name=f"layer_{self.hidden_layers - 1}")(hidden)
+        
+        logits = self.get_logits_argument(hidden)
+        return logits, hidden
+
+    def get_logits_argument(self, argument):
+        '''
+        Assigns logits to an argument.
+        '''
+        logits = tf.keras.layers.Dense(self.hidden_size, activation='relu', name='classifier_judge_0')(argument)
+        logits = tf.keras.layers.Dense(1, name='classifier_judge_1')(logits)
+        return logits
+
+    def final_loss(self, rep_argu_list):
+        '''
+        Computes the final loss and final logits of the debates using all arguments presented.
+        '''
+        average_argu = tf.reduce_mean(tf.stack(rep_argu_list, axis=-1), axis=-1)
+        final_logit = self.get_logits_argument(average_argu)
+        final_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.labels, logits=final_logit))
+        
+        return final_loss, final_logit
+
+    def set_query_embeddings(self, query_subject, query_relation, query_object):
+        '''
+        Sets the judge's query information to the placeholders used by trainer.
+        '''
+        self.query_subject_embedding = tf.nn.embedding_lookup(self.entity_lookup_table, query_subject)
+        self.query_relation_embedding = tf.nn.embedding_lookup(self.relation_lookup_table, query_relation)
+        self.query_object_embedding = tf.nn.embedding_lookup(self.entity_lookup_table, query_object)
